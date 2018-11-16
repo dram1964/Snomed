d92ded2eb2bb4cb0882b928031b6666bb33e3847
David Ramlakhan <dram1964@gmail.com>
2018-11-16 17:21:44 +0000

    First Commit

---
+++ script/generate_schema.pl
@@ -0,0 +1,13 @@
+use DBIx::Class::Schema::Loader qw/ make_schema_at /;
+
+make_schema_at(
+    'EHRS_Snomed::Schema',
+    {
+        debug          => 1,
+        dump_directory => './lib',
+    },
+    [
+        'dbi:ODBC:DSN=CRIU_EHRS_Snomed',
+        'dr00', 'letmein', { LongReadLen => 180, LongTruncOk => 1 }
+    ],
+);
